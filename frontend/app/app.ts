cb9eeac4c7d0d702810f0e1d3760e0634c3315a2
wenming <wenming@xiaoheiban.cn>
2019-09-11 15:32:54 +0800

    支持i18n

--- frontend/app/app.ts
+++ frontend/app/app.ts
@@ -11,17 +11,3 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err))
-
-
-// // Declare app level module which depends on views, and core components
-// angular.module('myApp', [
-//   'ngRoute',
-//   'myApp.view1',
-//   'myApp.view2',
-//   'myApp.version'
-// ]).
-// config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-//   $locationProvider.hashPrefix('!');
-//
-//   $routeProvider.otherwise({redirectTo: '/view1'});
-// }]);
